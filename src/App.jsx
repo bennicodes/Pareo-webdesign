3d18adf4ddb16712c1fcd887669add024be26cd5
Benedict Notoane <benni@MacBookPro.lan>
2025-07-13 14:33:20 +0200

    added media queries to shot descriptions

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import styles from "./App.module.css";
 import Button from "./components/Button/Button.jsx";
 import ContactForm from "./components/ContactForm/ContactForm.jsx";
+import Description from "./components/Description/Description.jsx";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import About from "./pages/About/About.jsx";
 import Contact from "./pages/Contact/Contact.jsx";
@@ -18,10 +19,10 @@ function App() {
             synlig på nettet.
           </h3>
         </div>
-        <div className={styles.actionContainer}>
+        <div className={styles.headerDescriptionContainer}>
           <Button classname={styles.contactButton}>Kontakt Oss</Button>
-          <div className={styles.descriptionContainer}>
-            <p className={styles.description}>
+          <div className={styles.headerDescription}>
+            <p className={styles.descriptionText}>
               Har du spørsmål? Send en uforpliktende henvendelse. Så tar vi
               kontakt så fort som mulig.
             </p>
@@ -34,39 +35,7 @@ function App() {
           <p>
             Vi starter med å bli kjent med deg og din bedrift. Dette innebærer :
           </p>
-          <ul className={styles.description}>
-            <li className={styles.descriptionText}>
-              <h3 className={styles.descriptionHeading}>
-                Strategi og innsikt:
-              </h3>
-              <p>
-                Vi setter oss inn i dine mål, utfordringer og visjoner. Sammen
-                definerer vi hva nettsiden skal oppnå og hvem målgruppen er. Så
-                lager vi en realistisk plan som passer din tidsramme.
-              </p>
-            </li>
-            <li
-              className={`${styles.descriptionText} ${styles.middleDescription}`}
-            >
-              <h3 className={styles.descriptionHeading}>
-                Design og utvikling:
-              </h3>
-              <p>
-                Vi skaper en nettside som både ser bra ut og fungerer optimalt.
-                Designet reflekterer din merkevare og gir en helhetlig visuell
-                identitet. Vi prioriterer enkel navigasjon, mobilvennlig design
-                og en god opplevelse for kundene dine.
-              </p>
-            </li>
-            <li className={styles.descriptionText}>
-              <h3 className={styles.descriptionHeading}>Teknisk utvikling:</h3>
-              <p>
-                Nettsiden bygges på en moderne plattform som sikrer rask
-                lastetid, stabilitet og god sikkerhet. Dette gjør det enkelt å
-                videreutvikle og oppgradere nettsiden hvis det trengs.
-              </p>
-            </li>
-          </ul>
+          <Description />
         </section>
         {/* ---------------------------- */}
         <section className={styles.contactSection}>
---
+++ src/components/Description/Description.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from "./Description.module.css";
+
+const Description = () => {
+  return (
+    <ul className={styles.description}>
+      <li className={styles.descriptionText}>
+        <h3 className={styles.descriptionHeading}>Strategi og innsikt:</h3>
+        <p>
+          Vi setter oss inn i dine mål, utfordringer og visjoner. Sammen
+          definerer vi hva nettsiden skal oppnå og hvem målgruppen er. Så lager
+          vi en realistisk plan som passer din tidsramme.
+        </p>
+      </li>
+      <li className={`${styles.descriptionText} ${styles.middleDescription}`}>
+        <h3 className={styles.descriptionHeading}>Design og utvikling:</h3>
+        <p>
+          Vi skaper en nettside som både ser bra ut og fungerer optimalt.
+          Designet reflekterer din merkevare og gir en helhetlig visuell
+          identitet. Vi prioriterer enkel navigasjon, mobilvennlig design og en
+          god opplevelse for kundene dine.
+        </p>
+      </li>
+      <li className={styles.descriptionText}>
+        <h3 className={styles.descriptionHeading}>Teknisk utvikling:</h3>
+        <p>
+          Nettsiden bygges på en moderne plattform som sikrer rask lastetid,
+          stabilitet og god sikkerhet. Dette gjør det enkelt å videreutvikle og
+          oppgradere nettsiden hvis det trengs.
+        </p>
+      </li>
+    </ul>
+  );
+};
+
+export default Description;
