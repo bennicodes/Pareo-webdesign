d923a89ed044b04e7465702eb192054c7187baa7
Benedict Notoane <benni@MacBookPro.lan>
2025-09-04 23:14:06 +0200

    fix button styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
-import App from "./App.jsx";
 import "./index.css";
 import { router } from "./Routes/Routes.jsx";
 
